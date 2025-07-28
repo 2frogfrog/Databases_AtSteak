84db757296f4503768ee830467e2529cfb7bc411
PanHands <Swordsmith.nh@gmail.com>
2025-07-28 15:00:28 -0700

    Modified the ddl script to have recipe ingredients listed correctly in relation to recipes. This is only so I could see what was wrong and if my queries still worked. Renzo is working on a cleaned up version of the data insertion at the moment of writing.

--- At_Steak_SQL_Script.sql
+++ At_Steak_SQL_Script.sql
@@ -151,17 +151,17 @@ ON DUPLICATE KEY UPDATE
  * Sample data for the table.
  * Assume Calories is calories per serving. 
  */
-INSERT INTO Recipes (RName, Servings, Calories) VALUES
-('Spaghetti Bolognese', 4, 500),
-('Chicken Caesar Salad', 2, 400),
-('Beef Tacos', 3, 300),
-('Vegetable Stir Fry', 2, 250),
-('Grilled Salmon', 2, 240),
-('Cheese Omelette', 1, 300),
-('Mushroom Risotto', 4, 420),
-('BBQ Chicken Pizza', 6, 400),
-('Lentil Soup', 3, 300),
-('Pancakes with Syrup', 2, 500);
+INSERT INTO Recipes (RName, Servings, Calories) VALUES 
+  ('Chicken Soup', 4, 150),
+  ('Omelette', 2, 250),
+  ('Creamy Pasta', 4, 600),
+  ('Mushroom Risotto', 4, 450),
+  ('BBQ Chicken Pizza', 6, 300),
+  ('Lentil Soup', 4, 180),
+  ('Pancakes with Syrup', 3, 350),
+  ('Vegetable Stir Fry', 4, 220),
+  ('Grilled Salmon', 2, 400),
+  ('Cheese Omelette', 2, 280);
 
 
 -- Ingrediant table sample data.
