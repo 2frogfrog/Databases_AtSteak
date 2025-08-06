196c53c52bcd4b3471943319947a6f2ff41ac474
Abby <2frogfrog@gmail.com>
2025-08-05 20:08:30 -0700

    Updated front end

--- Avg price of each recipe.sql
+++
@@ -1,8 +0,0 @@
--- Average price of each recipe
-SELECT
-recipes.RName,
-avg(food.price) AS AVG_Cost_Per_Recipe
-FROM recipes JOIN recipeingredients ON recipes.R_ID = recipeingredients.R_ID
-JOIN ingredients ON recipeingredients.Ingredient_ID = ingredients.Ingredient_ID
-JOIN food ON ingredients.FName = food.FName
-GROUP BY recipes.RName
