e3c3200f22a0a6e54a32af077db0760e2be86664
Renzo <149926085+Scizzanne@users.noreply.github.com>
2025-08-20 09:45:52 -0700

    changed query to show recipes only

--- backend/viewSmallServingRecipes.js
+++ backend/viewSmallServingRecipes.js
@@ -2,18 +2,9 @@ const pool = require('./DBConnection');
 
 async function viewSmallServingRecipes() {
 
-    const [rows] = await pool.query('SELECT DISTINCT\n' +
-    'FName FROM ingredients\n' +
-    'WHERE ingredients.Ingredient_ID IN (\n' +
-        'SELECT recipeingredients.Ingredient_ID\n' +
-    'FROM recipeingredients\n' +
-    'JOIN recipes ON recipeingredients.R_ID = recipes.R_ID\n' +
-    'WHERE recipes.Servings = 1\n' +
-    'UNION\n' +
-    'SELECT recipeingredients.Ingredient_ID\n' +
-    'FROM recipeingredients\n' +
-    'JOIN recipes ON recipeingredients.R_ID = recipes.R_ID\n' +
-    'Where recipes.Servings = 2);');
+    const [rows] = await pool.query('SELECT DISTINCT RName\n' +
+    'FROM recipes r\n' +
+    'WHERE r.Servings IN (1, 2);');
     return rows;
 }
 module.exports = viewSmallServingRecipes;
