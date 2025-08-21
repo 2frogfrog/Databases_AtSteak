27be4781c6fd12963975f34e5eb1c54bf2213f54
Noah Herd <134461753+PanHands@users.noreply.github.com>
2025-08-21 14:00:47 -0700

    Update DBConnection.js

--- backend/DBConnection.js
+++ backend/DBConnection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: "localhost",
-    user: "root",
+    user: "",
     password: "",
     database: "at_steak",
     connectionLimit: 10,
